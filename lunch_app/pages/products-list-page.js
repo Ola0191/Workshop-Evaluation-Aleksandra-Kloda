5183c24d8b63b630fa529b4552d559ce1063ba83
Natalia Topczewska <topczewskanatlia.n@gmail.com>
2024-08-26 13:09:19 +0200

    Prepared workspace

---
+++ lunch_app/pages/products-list-page.js
@@ -0,0 +1,8 @@
+export class ProductsListPage {
+  constructor(page) {
+    this.page = page;
+    this.itemNameDiv = page.locator('div[class="inventory_item_name"]');
+    this.itemPriceDiv = page.locator('div[class="inventory_item_price"]');
+  }
+
+}
